d7cf0079caac65ae7966620e6b38756af44b6797
Gizem Çakar <gizemcakar.es@gmail.com>
2025-01-03 00:38:37 +0300

    sales by match solution

---
+++ sales by match/salesByMatch.js
@@ -0,0 +1,16 @@
+
+let ar = [1, 2, 1, 2, 1, 3, 2];
+let n = ar.length;
+
+let pairs = function sockMerchant(n, ar) {
+    let counts = {};
+    for (var i = 0; i < ar.length; i++){
+        counts[ar[i]] = (counts[ar[i]] + 1) || 1;
+    }
+    let pair = 0;
+    for(let x in counts){
+        pair += parseInt(counts[x]/2)
+    }
+    return pair
+}
+document.getElementById("pairs").innerHTML = pairs(n, ar);

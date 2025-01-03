b41a9d3d251691a765c5fb58f1f81da9d9deff29
Gizem Çakar <gizemcakar.es@gmail.com>
2025-01-04 01:46:31 +0300

    minimum page number to be turned calculated

---
+++ drawing book/drawingBook.js
@@ -0,0 +1,25 @@
+
+let minimumPageNumber = function pageCount(n, p){
+    let turnedPageFromBeginning = 0;
+    for(let i=1; i<p; i=i+2){
+        turnedPageFromBeginning++;       
+    }
+    let turnedPageFromLastPage = 0;
+    let z = n
+    if(n % 2 === 1){
+        z = n-1
+    }
+    for(let i=z; i>p; i=i-2){
+        turnedPageFromLastPage++        
+    }
+    let minimumPageTurned = 0;
+    if(turnedPageFromBeginning < turnedPageFromLastPage){
+        minimumPageTurned = turnedPageFromBeginning
+    }
+    else{
+        minimumPageTurned = turnedPageFromLastPage;
+    }
+    return minimumPageTurned;
+}
+
+document.getElementById("minimumPage").innerHTML = minimumPageNumber(6,2)

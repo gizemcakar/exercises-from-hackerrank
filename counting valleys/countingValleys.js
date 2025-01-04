346d6a3cec3ec9ccb22658091a3ef3e56dc982c0
Gizem Çakar <gizemcakar.es@gmail.com>
2025-01-05 00:32:24 +0300

    valley number calculation

---
+++ counting valleys/countingValleys.js
@@ -0,0 +1,20 @@
+let path = ["U", "D", "D", "D", "U", "D", "U", "U"];
+let steps = path.length;
+
+let countingValleys = function(steps, path){
+    let seaLevel = 0;
+    let numberOfValley = 0;
+    for(let i=0; i<path.length; i++){
+        if(path[i] === "D"){
+            seaLevel--
+        }
+        else{
+            seaLevel++
+            if(seaLevel === 0){
+                numberOfValley++
+            }
+        }        
+    }
+    return numberOfValley;
+}
+document.getElementById("countingValleys").innerHTML = countingValleys(steps, path);

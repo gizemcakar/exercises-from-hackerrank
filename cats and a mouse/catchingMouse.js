68d96e6be6e9f04d45c2a9e9d28decf23cafb746
Gizem Çakar <gizemcakar.es@gmail.com>
2025-01-08 23:51:45 +0300

    function name was set.

--- cats and a mouse/catchingMouse.js
+++ cats and a mouse/catchingMouse.js
@@ -1,7 +1,7 @@
 let x = 2;
 let y = 5;
 let z = 4;
-let catchingMouse = function catAndMouse(x, y, z){
+function catAndMouse(x, y, z){
     let whoCatchesMouse = 0;
     let distancefromA = Math.abs(x-z);
     let distancefromB = Math.abs(y-z);
@@ -17,4 +17,4 @@ let catchingMouse = function catAndMouse(x, y, z){
     return whoCatchesMouse;   
 }
 
-document.getElementById("catchingMouse").innerHTML = catchingMouse(x, y, z);
+document.getElementById("catchingMouse").innerHTML = catAndMouse(x, y, z);

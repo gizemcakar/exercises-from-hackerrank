5e1bea2899fe9a9376d8d1a1957405a6d851ff28
Gizem Çakar <gizemcakar.es@gmail.com>
2025-01-21 22:52:45 +0300

    correct solution added

--- save the prisoner/saveThePrisoner.js
+++ save the prisoner/saveThePrisoner.js
@@ -1,16 +1,9 @@
 // n prisoner number, m sugar number, s chair number to begin from.
 // s'yi en son verilen seker olarak dusunup dongu ona gore devam edecek. 1,2,3,4,1,2, m'e kadar bu şekilde gider.
-// 0'da seker bittiginden dolayi dongu 1'de bitip son seker kime gidecek bulunur.
-function saveThePrisoner(n, m, s) {
 
-    for(let i=m; i>1; i--){
-        if(s === n){
-           s=1; 
-        }
-        else{
-            s++
-        } 
-    }
-    return s
+function saveThePrisoner(n, m, s) {
+    let sugar = m+s-1 ;
+    let final = sugar>n ? sugar%n : sugar
+    return final===0? n : final
 }
 document.querySelector("#savePrisoner").innerHTML = saveThePrisoner(5, 2, 2);
